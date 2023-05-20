26f1e405c54dceee3477e00751923b3af97e2c8e
brumoreiras <bruhnno.ms@hotmail.com>
2023-05-20 11:48:38 -0300

    Criando API

---
+++ point/API/src/app.js
@@ -0,0 +1,55 @@
+
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const cadastroDoUsuario = [
+    {
+        id: '',
+        "eMail": '',
+        "senha": ''
+    },
+]
+
+app.get('/', (req, res) => {
+    res.send("Seja bem vindo")
+})
+
+app.get('/crie-sua-conta', (req, res) => {
+    res.status(200).json(cadastroDoUsuario)
+})
+
+app.get('/crie-sua-conta/:id', (req, res) => {
+    let { id } = req.params
+    let index = buscarUsuario(id)
+    res.status(200).json(cadastroDoUsuario[index])
+})
+
+app.post('/crie-sua-conta', (req, res) => {
+    cadastroDoUsuario.push(req.body)
+    res.status(201).send('Cadastro realizado com sucesso')
+})
+
+app.put('/crie-sua-conta/:id', (req, res) => {
+    let { id } = req.params
+    let index = buscarUsuario(id)
+    cadastroDoUsuario[index].eMail = req.body.eMail
+    cadastroDoUsuario[index].senha = req.body.senha
+    res.status(200).json(cadastroDoUsuario)
+
+})
+
+app.delete('/crie-sua-conta/:id', (req, res) => {
+    let { id } = req.params
+    let index = buscarUsuario(id)
+    cadastroDoUsuario.splice(index, 1)
+    res.send(`Usuário ${id} foi removido com sucesso!`)
+})
+
+function buscarUsuario(id) {
+    return cadastroDoUsuario.findIndex(cadastroDoUsuario => cadastroDoUsuario.id == id)
+}
+
+export default app
