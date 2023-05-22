7570c550961616eedc01b1418692453b4cff4f63
brumoreiras <bruhnno.ms@hotmail.com>
2023-05-21 22:40:22 -0300

    Atualizando API

--- MyPoint/API/src/app.js
+++ MyPoint/API/src/app.js
@@ -1,30 +1,31 @@
-
 import express from 'express'
 
 const app = express()
 
 app.use(express.json())
 
-const cadastroDoUsuario = [
-    {
-        id: '',
-        "eMail": '',
-        "senha": ''
-    },
-]
+const cadastroDoUsuario = []
 
 app.get('/', (req, res) => {
-    res.send("Seja bem vindo")
+    res.send("Seja bem-vindo")
 })
 
 app.get('/crie-sua-conta', (req, res) => {
-    res.status(200).json(cadastroDoUsuario)
+    if (cadastroDoUsuario.length === 0) {
+        res.status(200).send('Nenhum dado cadastrado')
+    } else {
+        res.status(200).json(cadastroDoUsuario)
+    }
 })
 
 app.get('/crie-sua-conta/:id', (req, res) => {
-    let { id } = req.params
-    let index = buscarUsuario(id)
-    res.status(200).json(cadastroDoUsuario[index])
+    const { id } = req.params
+    const index = buscarUsuario(id)
+    if (index !== -1) {
+        res.status(200).json(cadastroDoUsuario[index])
+    } else {
+        res.status(404).send('Usuário não encontrado')
+    }
 })
 
 app.post('/crie-sua-conta', (req, res) => {
@@ -33,23 +34,30 @@ app.post('/crie-sua-conta', (req, res) => {
 })
 
 app.put('/crie-sua-conta/:id', (req, res) => {
-    let { id } = req.params
-    let index = buscarUsuario(id)
-    cadastroDoUsuario[index].eMail = req.body.eMail
-    cadastroDoUsuario[index].senha = req.body.senha
-    res.status(200).json(cadastroDoUsuario)
-
+    const { id } = req.params
+    const index = buscarUsuario(id)
+    if (index !== -1) {
+        cadastroDoUsuario[index].email = req.body.email
+        cadastroDoUsuario[index].senha = req.body.senha
+        res.status(200).json(cadastroDoUsuario)
+    } else {
+        res.status(404).send('Usuário não encontrado')
+    }
 })
 
 app.delete('/crie-sua-conta/:id', (req, res) => {
-    let { id } = req.params
-    let index = buscarUsuario(id)
-    cadastroDoUsuario.splice(index, 1)
-    res.send(`Usuário ${id} foi removido com sucesso!`)
+    const { id } = req.params
+    const index = buscarUsuario(id)
+    if (index !== -1) {
+        cadastroDoUsuario.splice(index, 1)
+        res.send(`Usuário ${id} foi removido com sucesso!`)
+    } else {
+        res.status(404).send('Usuário não encontrado')
+    }
 })
 
 function buscarUsuario(id) {
-    return cadastroDoUsuario.findIndex(cadastroDoUsuario => cadastroDoUsuario.id == id)
+    return cadastroDoUsuario.findIndex(usuario => usuario.id == id)
 }
 
 export default app
