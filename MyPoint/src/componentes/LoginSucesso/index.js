9229c7eaf626c39d6ec1b4ea34e48df1bf2fac8a
brumoreiras <bruhnno.ms@hotmail.com>
2023-06-19 15:52:08 -0300

    Criando função deletar conta

--- MyPoint/src/componentes/LoginSucesso/index.js
+++ MyPoint/src/componentes/LoginSucesso/index.js
@@ -1,21 +1,26 @@
 import './LoginSucesso.css'
 import { useLocation, Link } from 'react-router-dom'
-
+import Botao from "../Botao"
 
 const LoginSucesso = ({ location }) => {
-
+    async function deleteConta(){
+        
+    }
 
     const { state } = useLocation();
     const name = state ? state.name : "";
 
     return (
-        <div className='login_sucesso'>
-            <h1>Bem-vindo ao MyPoint: Descubra o Melhor da Sua Cidade!</h1>
-            <h2>Olá {name}</h2>
-            <div className='botao_sair'>
-                <Link to='/'>Sair</Link>
+        <>
+            <div className='login_sucesso'>
+                <h1>Bem-vindo ao MyPoint: Descubra o Melhor da Sua Cidade!</h1>
+                <h2>Olá {name}</h2>
+                <div className='botao_sair'>
+                    <Link to='/'>Sair</Link>
+                </div>
+                <Botao onChage={deleteConta}>Deletar</Botao>
             </div>
-        </div>
+        </>
     )
 }
 export default LoginSucesso
